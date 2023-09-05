c7eb74fff6e855abb67511ce217a47f32d1ca4f8
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-05 11:08:54 +0500

    end

--- ex17/Program.cs
+++ ex17/Program.cs
@@ -10,6 +10,44 @@ namespace ex17
     {
         static void Main(string[] args)
         {
+            char startSymbol = '(';
+            char endSymbol = ')';
+            int maxDepthSymbols = 0;
+            int symbolsEquality = 0;
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == startSymbol)
+                {
+                    symbolsEquality++;
+
+                    if (symbolsEquality > maxDepthSymbols)
+                    {
+                        maxDepthSymbols = symbolsEquality;
+                    }
+
+                }
+                else if (text[i] == endSymbol)
+                {
+                    symbolsEquality--;
+
+                    if (symbolsEquality < 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (symbolsEquality != 0)
+            {
+                Console.WriteLine("Некорректная запись");
+            }
+            else
+            {
+                Console.WriteLine($"Запись корректна. Глубина вложенности скобки равна {maxDepthSymbols}");
+            }
         }
     }
 }
